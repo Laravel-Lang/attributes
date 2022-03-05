bc395b275a5874e3dd8d86c86efe14875a5bfbaf
Niclas Bartsch <74824542+typingbeaver@users.noreply.github.com>
2022-03-05 19:21:24 +0100

    [de] Update translations

---
+++ excludes/de.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Exclusion list
+|--------------------------------------------------------------------------
+|
+| This is a list of exclusions for words or phrases where the original
+| form of the word has the same spelling in a given language.
+|
+| This list contains values.
+|
+*/
+
+return [
+    'Filter',
+    'Minute',
+    'Name',
+    'Test Name',
+    'Text',
+];
